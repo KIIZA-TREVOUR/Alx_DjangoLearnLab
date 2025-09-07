4decc1021969ef0adbef349bb63fbd736f95b911
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-09-07 13:54:54 +0300

    added views and templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 # Function-based view to list all books
 def list_books(request):
