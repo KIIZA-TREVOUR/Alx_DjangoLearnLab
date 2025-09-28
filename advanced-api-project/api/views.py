dac5a136c065735c60d439ed26e159e990674b27
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-09-28 11:44:29 +0300

    modified Booklist view to handle filtering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,21 +1,27 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-from rest_framework import filters  # for search and ordering
-from django_filters.rest_framework import DjangoFilterBackend  # for filtering
+from rest_framework import filters as drf_filters  
+from django_filters import rest_framework as filters  
 from .models import Book
 from .serializers import BookSerializer
 
-# Show all books - anyone can see with  filtering, searching, and ordering
+# Supports filtering, searching, and ordering
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     # Add filtering, searching, and ordering
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [
+        filters.DjangoFilterBackend,        
+        drf_filters.SearchFilter,           
+        drf_filters.OrderingFilter
+    ]
+
     # Fields you can filter by (exact match)
     filterset_fields = ['title', 'publication_year', 'author']
     # Fields you can search in (text search)
     search_fields = ['title', 'author__name']
+    
     # Fields you can order by
     ordering_fields = ['title', 'publication_year']
     ordering = ['title']  # default order
