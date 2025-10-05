48e81fff8b529da62cb127b9d6eb7748322c2c86
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-10-05 23:05:28 +0300

    added taggit to settings

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post, Comment, Tag
+from .models import Post, Comment
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -16,51 +16,16 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
-    
-class ProfileEditForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('email')
-        widgets = {
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-        }
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(
-        required=False,
-        help_text="Enter tags separated by commas (e.g., django, python, web)",
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'django, python, blog'})
-    )
-
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'tags']  # ← tags is now handled by taggit
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+            'tags': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'django, python, blog'}),
         }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance.pk:
-            # Pre-populate tags field with existing tags
-            self.fields['tags'].initial = ', '.join([tag.name for tag in self.instance.tags.all()])
-
-    def save(self, commit=True):
-        post = super().save(commit=False)
-        if commit:
-            post.save()
-            # Handle tags
-            tag_names = self.cleaned_data.get('tags', '')
-            if tag_names:
-                tag_list = [tag.strip().lower() for tag in tag_names.split(',') if tag.strip()]
-                post.tags.clear()
-                for tag_name in tag_list:
-                    tag, created = Tag.objects.get_or_create(name=tag_name)
-                    post.tags.add(tag)
-            else:
-                post.tags.clear()
-        return post
 
 class CommentForm(forms.ModelForm):
     class Meta:
