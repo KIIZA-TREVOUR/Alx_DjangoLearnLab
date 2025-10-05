91b6e9ea96140e8f1f808c1a778f7bb2d13ab866
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-10-05 20:12:39 +0300

    Add PostForm for creating and editing blog posts

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Post
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -22,4 +23,13 @@ class ProfileEditForm(forms.ModelForm):
         fields = ('email')
         widgets = {
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
