aad49020c5ac9510295ec41e9b44ed1864f29432
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-10-05 22:34:20 +0300

    Update PostForm to handle comma-separated tags with get_or_create logic

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Comment, Tag
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -26,13 +26,41 @@ class ProfileEditForm(forms.ModelForm):
         }
 
 class PostForm(forms.ModelForm):
+    tags = forms.CharField(
+        required=False,
+        help_text="Enter tags separated by commas (e.g., django, python, web)",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'django, python, blog'})
+    )
+
     class Meta:
         model = Post
         fields = ['title', 'content']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            # Pre-populate tags field with existing tags
+            self.fields['tags'].initial = ', '.join([tag.name for tag in self.instance.tags.all()])
+
+    def save(self, commit=True):
+        post = super().save(commit=False)
+        if commit:
+            post.save()
+            # Handle tags
+            tag_names = self.cleaned_data.get('tags', '')
+            if tag_names:
+                tag_list = [tag.strip().lower() for tag in tag_names.split(',') if tag.strip()]
+                post.tags.clear()
+                for tag_name in tag_list:
+                    tag, created = Tag.objects.get_or_create(name=tag_name)
+                    post.tags.add(tag)
+            else:
+                post.tags.clear()
+        return post
 
 class CommentForm(forms.ModelForm):
     class Meta:
